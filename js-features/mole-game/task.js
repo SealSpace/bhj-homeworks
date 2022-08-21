6362edba90c3285480d8397daa0a7f4654c61b98
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-08-21 18:50:32 +0300

    Update task.js

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,33 @@
+const deaths = document.getElementById("dead");
+const losts = document.getElementById("lost");
+
+function checkHole() {
+
+    const currentHole = this;
+
+    if (currentHole.className.includes("hole_has-mole")) {
+        deaths.textContent = +deaths.textContent + 1;
+    } else {
+        losts.textContent = +losts.textContent + 1;
+    }
+
+    if (+deaths.textContent === 10) {
+        alert("Победа!");
+        deaths.textContent = 0;
+        losts.textContent = 0;
+    }
+
+    if (+losts.textContent === 5) {
+        alert("Вы проиграли.");
+        deaths.textContent = 0;
+        losts.textContent = 0;
+    }
+}
+
+function getHole(index) {
+    return document.getElementById(`hole${index}`);
+}
+
+for (i = 1; 9; i++) {
+    getHole(i).onclick = checkHole;
+}
