a20b5568371a094aef0838b2f8fb8835861430af
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-08-21 18:38:04 +0300

    Update task.js

---
+++ js-features/cookie-clicker/task.js
@@ -0,0 +1,14 @@
+const clickCounter = document.getElementById('clicker__counter');
+
+const cookieImg = document.getElementById('cookie');
+
+cookieImg.onclick = function() {
+
+	if(cookieImg.width == 200) {
+		cookieImg.width = 250;
+	} else {
+		cookieImg.width = 200
+	};
+  
+	clickCounter.textContent = +clickCounter.textContent + 1;
+}
