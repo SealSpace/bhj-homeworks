2f43d980689b0efe3f5cfe784652548c227269ed
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-08-25 20:10:39 +0300

    Update task.js

---
+++ element-search/slider/task.js
@@ -0,0 +1,25 @@
+const slider = Array.from(document.getElementsByClassName('slider__item'))
+
+const arrowLeft = document.querySelector('.slider__arrow_prev')
+
+const arrowRight = document.querySelector('.slider__arrow_next')
+
+
+
+arrowRight.onclick = () => {
+
+    let sliderIndex = slider.findIndex(item => item.className === 'slider__item slider__item_active');
+
+    slider[sliderIndex].classList.remove('slider__item_active')
+
+    slider[sliderIndex == slider.length - 1 ? 0 : sliderIndex + 1].classList.add('slider__item_active')
+};
+
+arrowLeft.onclick = () => {
+
+    sliderIndex = slider.findIndex(item => item.className === 'slider__item slider__item_active');
+
+    slider[sliderIndex].classList.remove('slider__item_active')
+
+    slider[sliderIndex == 0 ? slider.length - 1 : sliderIndex - 1].classList.add('slider__item_active')
+};
