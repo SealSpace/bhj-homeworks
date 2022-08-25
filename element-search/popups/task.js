f99094f80734bbf387de37f2e790ecba9ceceef9
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-08-25 20:02:10 +0300

    Update task.js

---
+++ element-search/popups/task.js
@@ -0,0 +1,22 @@
+// окно #modal_main
+const openPop = document.getElementById('modal_main');
+openPop.classList.add('modal_active');
+
+// закрытие активного окна по нажатию на его элемент с классом modal__close
+const closePop = document.querySelectorAll('.modal__close_times');
+for (let i = 0; i < closePop.length; i++){
+closePop[i].onclick = function clossTimes() {
+    this.closest('.modal').classList.remove('modal_active');
+    
+};
+
+};
+
+// нажатие на элемент с классом show-success появление окна #modal_success
+const showSuccesss = document.querySelector('.show-success');
+const modal = document.getElementById('modal_success')
+
+showSuccesss.onclick = () => {
+    openPop.classList.remove('modal_active')
+    modal.classList.add('modal_active')
+};
