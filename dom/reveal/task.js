bd694f4c76f76ccee5e40f688cdf190750f0c1b2
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-05 23:37:30 +0300

    Update task.js

---
+++ dom/reveal/task.js
@@ -0,0 +1,19 @@
+const reveal = Array.from(document.getElementsByClassName('reveal'));
+let windowHeight = window.innerHeight
+console.log(window.innerHeight)
+
+
+window.addEventListener('scroll', isVisible)
+
+function isVisible() {
+    reveal.forEach(item => {
+        let top = item.getBoundingClientRect().y;
+
+        console.log(top)
+        if (top < windowHeight && top > 0) {
+            item.classList.add('reveal_active');
+        } else {
+            item.classList.remove('reveal_active');
+        };
+    });
+};
