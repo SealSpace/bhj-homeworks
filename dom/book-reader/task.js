0011d31c2f940db3ea40b0f1c73f5b7ec83e46aa
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-05 23:42:57 +0300

    Update task.js

---
+++ dom/book-reader/task.js
@@ -0,0 +1,24 @@
+const fontSizeAll = Array.from(document.getElementsByClassName("font-size"));
+const book = document.querySelector('.book__content');
+console.log(fontSizeAll)
+
+fontSizeAll.forEach(item => item.addEventListener("click", (event) => {
+    event.preventDefault();
+   
+    fontSizeAll.forEach(elem => elem.classList.remove("font-size_active"))
+         
+    if (item.classList.contains("font-size_small")) {
+        book.classList.remove("font-size_big")
+        book.classList.add("font-size_small")
+    } else if (item.classList.contains("font-size_big")) {
+        book.classList.remove("font-size_small")
+        book.classList.add("font-size_big")
+
+    } else {
+        book.classList.remove("font-size_big")
+        book.classList.remove("font-size_small")
+    };
+    item.classList.add("font-size_active")
+    
+   
+}));
