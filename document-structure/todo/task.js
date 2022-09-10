aa47db367fead3474431253e4760312d8518ac87
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-10 09:09:53 +0300

    Update task.js

---
+++ document-structure/todo/task.js
@@ -0,0 +1,18 @@
+const taskInput = document.getElementById('task__input');
+const tasksAdd = document.getElementById('tasks__add');
+const tasksList = document.getElementById('tasks__list');
+
+tasksAdd.addEventListener('click', (event) => {
+    event.preventDefault();
+    // console.log(taskInput.value.trim())
+    if (taskInput.value.length > 0 && taskInput.value.trim() !== '') {
+        tasksList.insertAdjacentHTML('beforeEnd', `<div class="task"><div class="task__title">${taskInput.value}</div><a href="#" class="task__remove">&times;</a></div>`)
+        taskInput.value = " ";
+    };
+});
+
+tasksList.addEventListener('click', (event) => {
+    if (event.target.closest('.task')) {
+        event.target.closest('.task').remove();
+    };
+});
