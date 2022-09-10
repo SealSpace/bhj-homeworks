6afbdd496cd8dc2ec34c7fcf7da27900673bc763
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-09-10 09:06:57 +0300

    Update task.js

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,15 @@
+const a = Array.from(document.getElementsByClassName('has-tooltip'));
+a.forEach(item => {
+    item.onclick = function (e) {
+
+        e.preventDefault()
+        if (!(item.nextElementSibling && item.nextElementSibling.classList.contains('tooltip'))) {
+            // console.log('tik')
+            item.insertAdjacentHTML("afterEnd", '<div class="tooltip" style="left: 0; top: 0"></div>')
+        }
+        item.nextElementSibling.classList.toggle('tooltip_active');
+        item.nextElementSibling.textContent = item.title;
+        item.nextElementSibling.style.left = `${item.getBoundingClientRect().left}px`;
+        item.nextElementSibling.style.top = `${item.getBoundingClientRect().top + 25}px`;
+    };
+});
